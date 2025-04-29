7ff7c4e2149361e25ebbbe18d550d6bb6296abab
Mahfuz <aamahfuz.pro@gmail.com>
2025-04-30 04:35:24 +0600

    modules: modified food interface

--- src/modules/foods/food.interface.ts
+++ src/modules/foods/food.interface.ts
@@ -4,13 +4,15 @@ export interface IFoodItem {
   name: string; // Name of the food item
   price: number; // Price of the food item
   amount: number; // Amount (in kg or pieces)
+  image: string; // URL or path to the food item image
   category?: string; // Optional category of the food item
   description?: string; // Optional description of the food item
   createdAt?: Date; // Date the food item was created
   updatedAt?: Date; // Date the food item was last updated
 }
 
+// Extends Mongoose Document and includes all properties from IFoodItem
 export interface FoodItemDocument extends IFoodItem, mongoose.Document {
-  createdAt: Date;
+  createdAt: Date; // Ensuring these are set by Mongoose
   updatedAt: Date;
 }
--- src/modules/foods/food.model.ts
+++ src/modules/foods/food.model.ts
@@ -10,6 +10,7 @@ const FoodItemSchema: Schema = new Schema<FoodItemDocument>(
   {
     name: { type: String, required: true }, // Name of the food item (Required)
     price: { type: Number, required: true }, // Price of the food item (Required)
+    image: { type: String, required: false },
     amount: { type: Number, required: true }, // Amount (in kg or pieces, Required)
     category: { type: String, required: false }, // Optional category for the food item
     description: { type: String, required: false }, // Optional description of the food item
